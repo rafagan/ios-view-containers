a61c5ef7406a2774aeabdf2ebf487c3201f305c7
Rafagan Abreu <rafagan@guizion.com>
2017-11-29 11:13:22 -0200

    Célula customizada

---
+++ Container/Container/TableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewCell.swift
+//  Container
+//
+//  Created by Rafagan Abreu on 29/11/17.
+//  Copyright © 2017 Rafagan Abreu. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentTextLabel: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
